a778c9c99a6025cdf5afe61f0c1856e8d7eb06e0
Pham Ngoan <phngoan1999@gmail.com>
2024-07-28 22:53:36 +0700

    set up environment

---
+++ src/selenium/Topic_01.java
@@ -0,0 +1,24 @@
+package selenium;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Topic_01 {
+	
+  @Test
+  public void f() {
+	  String  projectPath = System.getProperty("user.dir");
+	  System.setProperty("webdriver.chrome.driver","./browerDriver/chromedriver.exe");
+	  WebDriver driver = new ChromeDriver();
+	  String baseUrl = "https://admin-demo.nopcommerce.com/login";
+	  System.out.print("-----------Test first-----------");
+	  driver.get(baseUrl);
+	  String title = driver.getTitle();
+	  System.out.println(title);
+	  Assert.assertEquals("Your store. Login", title);
+  }
+  
+}
